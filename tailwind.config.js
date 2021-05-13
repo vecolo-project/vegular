62a2ddce38ef5f1aba35f51da7be90d7619638c6
Nouu <noelarrieulacoste@yahoo.fr>
2021-05-13 13:09:03 +0200

    tailwind + angular material implmented

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const {guessProductionMode} = require("@ngneat/tailwind");
+
+module.exports = {
+  important: true,
+  prefix: '',
+  purge: {
+    enabled: guessProductionMode(),
+    content: [
+      './src/**/*.{html,ts}',
+    ]
+  },
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/line-clamp')],
+};
