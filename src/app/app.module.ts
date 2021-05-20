f10d56d576e1aa229d38786b108e2ea07acbe4ce
Swann <swann@devloup.dev>
2021-05-20 09:29:15 +0200

    add basic structure menu for mobile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import {AkitaNgDevtools} from '@datorama/akita-ngdevtools';
-import {FormBuilder} from '@angular/forms';
-import {CoreModule} from './core/core.module';
-import {ProfileModule} from './features/profile/profile.module';
-import {SharedModule} from './shared/shared.module';
-import {CommonModule} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+import { FormBuilder } from '@angular/forms';
+import { CoreModule } from './core/core.module';
+import { ProfileModule } from './features/profile/profile.module';
+import { SharedModule } from './shared/shared.module';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from './core/components/menu/menu.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent, MenuComponent],
   imports: [
     CommonModule,
     SharedModule,
@@ -25,7 +24,6 @@ import {CommonModule} from '@angular/common';
     AkitaNgDevtools.forRoot(),
   ],
   providers: [FormBuilder],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
