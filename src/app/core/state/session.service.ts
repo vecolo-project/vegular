e7b9a816fa47e4fa5e34d7fc369984346ed565d2
Swann <swann@devloup.dev>
2021-05-25 23:15:36 +0200

    add axios + test avec axios

--- src/app/core/state/session.service.ts
+++ src/app/core/state/session.service.ts
@@ -1,20 +1,25 @@
-import {Injectable} from '@angular/core';
-import {SessionStore} from './session.store';
-import {RouterNavigation} from '../router/router.navigation';
+import { Injectable } from '@angular/core';
+import { SessionStore } from './session.store';
+import { RouterNavigation } from '../router/router.navigation';
+import axios from 'axios';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class SessionService {
+  endPoint = 'http://localhost:4562/api';
 
-  constructor(private routerNavigation: RouterNavigation, private sessionStore: SessionStore) {
-  }
+  constructor(
+    private routerNavigation: RouterNavigation,
+    private sessionStore: SessionStore
+  ) {}
 
-  login(email: string, password: string): void {
-    if (password !== 'esgi') {
-      console.log('Invalid password !');
-      return;
-    }
-    this.sessionStore.setUser({email, username: 'Nospy'});
-    this.sessionStore.setToken('token-secret-jwt');
+  async login(email: string, password: string): Promise<void> {
+    const user = await axios.post(this.endPoint + '/auth/login', {
+      email: email,
+      password: password,
+    });
+    console.log(user);
+    this.sessionStore.setUser(user.data.user);
+    this.sessionStore.setToken(user.data.token);
     this.routerNavigation.gotoProfile();
   }
 
@@ -22,6 +27,4 @@ export class SessionService {
     this.sessionStore.logout();
     this.routerNavigation.gotoLogin();
   }
-
-
 }
