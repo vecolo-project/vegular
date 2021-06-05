f4df313ed3e4c8fbecb01ac57f016bef4b2f6554
Swann <swann@devloup.dev>
2021-06-05 18:12:23 +0200

    wrap snackbar with custom function

--- src/app/core/state/session.service.ts
+++ src/app/core/state/session.service.ts
@@ -1,36 +1,35 @@
-import {Injectable} from '@angular/core';
-import {SessionStore} from './session.store';
-import {RouterNavigation} from '../router/router.navigation';
-import {User} from '../../shared/models/user.model';
-import {HttpClientWrapper} from '../utils/httpClientWrapper';
-import {API_RESSOURCE_URI} from '../../shared/api-ressource-uri/api-ressource-uri';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { Injectable } from '@angular/core';
+import { SessionStore } from './session.store';
+import { RouterNavigation } from '../router/router.navigation';
+import { User } from '../../shared/models/user.model';
+import { HttpClientWrapper } from '../utils/httpClientWrapper';
+import { API_RESSOURCE_URI } from '../../shared/api-ressource-uri/api-ressource-uri';
+import { Snackbar } from '../../shared/snackbar/snakbar';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class SessionService {
   constructor(
     private routerNavigation: RouterNavigation,
     private sessionStore: SessionStore,
     private http: HttpClientWrapper,
-    private snackBar: MatSnackBar
-  ) {
-  }
+    private snackBar: Snackbar
+  ) {}
 
   async login(email: string, password: string): Promise<void> {
     try {
-      const response = await this.http.post<{ user: User, token: string }>(API_RESSOURCE_URI.LOGIN, {
-        email,
-        password
-      });
+      const response = await this.http.post<{ user: User; token: string }>(
+        API_RESSOURCE_URI.LOGIN,
+        {
+          email,
+          password,
+        }
+      );
       this.sessionStore.setUser(response.user);
       this.sessionStore.setToken(response.token);
       this.routerNavigation.gotoHome();
+      this.snackBar.success('vous êtes maintenant connecter');
     } catch (e) {
-      this.snackBar.open('Erreur de connexion : ' + e.error.error, '',
-        {
-          duration: 3000,
-          panelClass: ['bg-red-400']
-        });
+      this.snackBar.warnning('Erreur de connexion : ' + e.error.error);
     }
   }
 
---
+++ src/app/shared/snackbar/snakbar.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({ providedIn: 'root' })
+export class Snackbar {
+  constructor(private snackBar: MatSnackBar) {}
+  success(message: string, duration = 3000): void {
+    this.snackBar.open(message, '', {
+      duration,
+      panelClass: 'bg-green-500',
+      horizontalPosition: 'start',
+    });
+  }
+  danger(message: string, duration = 3000): void {
+    this.snackBar.open(message, '', {
+      duration,
+      panelClass: 'bg-red-500',
+      horizontalPosition: 'start',
+    });
+  }
+  warnning(message: string, duration = 3000): void {
+    this.snackBar.open(message, '', {
+      duration,
+      panelClass: 'bg-yellow-500',
+      horizontalPosition: 'start',
+    });
+  }
+}
