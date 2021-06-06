2b6ac1bb39acfbdf9ff68267fbc225cf0d913351
Nouu <noelarrieulacoste@yahoo.fr>
2021-06-06 17:12:04 +0200

    fix

--- src/app/features/users/users.module.ts
+++ src/app/features/users/users.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { UsersComponent } from './containers/users/users.component';
-import { UsersRoutingModule } from './user-routing.module';
-import { UserListComponent } from './components/user-list/user-list.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {UsersComponent} from './containers/users/users.component';
+import {UsersRoutingModule} from './user-routing.module';
+import {UserListComponent} from './components/user-list/user-list.component';
+import {SharedModule} from '../../shared/shared.module';
 
 @NgModule({
   declarations: [UsersComponent, UserListComponent],
-  imports: [UsersRoutingModule, CommonModule],
+  imports: [UsersRoutingModule, CommonModule, SharedModule ],
 })
-export class UsersModule {}
+export class UsersModule {
+}
