b565dda15cab80c3865ba07d7ef995ee3eb252f6
Swann <swann@devloup.dev>
2021-06-14 23:43:38 +0200

    add session query and fail

--- src/app/features/users/containers/users/users.component.ts
+++ src/app/features/users/containers/users/users.component.ts
@@ -1,31 +1,34 @@
-import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
-import {HashMap} from '@datorama/akita';
-import {User} from '../../../../shared/models/user.model';
-import {UsersQuery} from '../../store/users.query';
-import {UsersService} from '../../store/users.service';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HashMap } from '@datorama/akita';
+import { User } from '../../../../shared/models/user.model';
+import { UsersQuery } from '../../store/users.query';
+import { UsersService } from '../../store/users.service';
+import { SessionQuery } from '../../../../core/store/session.query';
 
 @Component({
   selector: 'app-user',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
   users: Observable<HashMap<User>>;
   usersIds: Observable<number[]>;
   usersLoading: Observable<boolean>;
 
-  constructor(private usersQuery: UsersQuery, private usersService: UsersService) {
+  constructor(
+    private usersQuery: UsersQuery,
+    private usersService: UsersService,
+    public sessionQuery: SessionQuery
+  ) {
     this.users = this.usersQuery.selectUsers$;
     this.usersIds = this.usersQuery.selectIds$;
     this.usersLoading = this.usersQuery.isLoading$;
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   getUsers(limit: number, offset: number): void {
     this.usersService.getUsers();
   }
-
 }
