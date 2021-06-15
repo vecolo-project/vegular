15dae07629db23b99c6802d16bd1f09452b2c531
Swann <swann@devloup.dev>
2021-06-15 08:52:59 +0200

    add formGroup

--- src/app/features/users/components/users-form/users-form.component.ts
+++ src/app/features/users/components/users-form/users-form.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HashMap } from '@datorama/akita';
+import { Observable } from 'rxjs';
+import { User } from '../../../../shared/models/user.model';
+import { UsersQuery } from '../../store/users.query';
 
 @Component({
   selector: 'app-users-form',
   templateUrl: './users-form.component.html',
-  styleUrls: ['./users-form.component.scss']
+  styleUrls: ['./users-form.component.scss'],
 })
 export class UsersFormComponent implements OnInit {
+  user: Observable<HashMap<User>>;
+  form: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fp: FormBuilder, private usersQuery: UsersQuery) {
+    this.form = fp.group({});
   }
 
+  ngOnInit(): void {
+    this.user = this.usersQuery.selectUsers$;
+    console.log(this.user);
+  }
 }
