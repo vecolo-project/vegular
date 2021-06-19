4dd40c7e3939fed7ccbbf9df907d626000b30f50
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2021-06-19 12:12:51 +0200

    add edit user in store

--- src/app/features/users/store/users.store.ts
+++ src/app/features/users/store/users.store.ts
@@ -1,19 +1,21 @@
-import { EntityState, EntityStore, StoreConfig } from '@datorama/akita';
-import { User } from '../../../shared/models/user.model';
-import { Injectable } from '@angular/core';
+import {EntityState, EntityStore, StoreConfig} from '@datorama/akita';
+import {User} from '../../../shared/models/user.model';
+import {Injectable} from '@angular/core';
 
 export interface UsersState extends EntityState<User, number> {
   count: number;
+  editUser: User;
 }
 
 export function createInitialState(): UsersState {
   return {
     count: 0,
+    editUser: undefined
   };
 }
 
-@Injectable({ providedIn: 'root' })
-@StoreConfig({ name: 'users' })
+@Injectable({providedIn: 'root'})
+@StoreConfig({name: 'users'})
 export class UsersStore extends EntityStore<UsersState, User> {
   constructor() {
     super(createInitialState());
