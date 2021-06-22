3c3695ae4bbc76d0637f3ff4bf775baaab36dc8e
Swann <swann@devloup.dev>
2021-06-22 19:16:33 +0200

    add more field

--- src/app/features/users/components/users-form/users-form.component.ts
+++ src/app/features/users/components/users-form/users-form.component.ts
@@ -21,6 +21,9 @@ export class UsersFormComponent implements OnInit, OnChanges {
   @Input()
   public user: User;
 
+  @Input()
+  public isAddMode: boolean;
+
   @Output()
   public retrieveEditUser = new EventEmitter<any>();
 
@@ -34,10 +37,17 @@ export class UsersFormComponent implements OnInit, OnChanges {
       fieldLastName: ['', [Validators.required]],
       fieldPassword: [''],
       fieldConfirmPassword: [''],
+      fieldBirthDate: ['', [Validators.required]],
+      fieldPseudo: ['', [Validators.required]],
+      fieldRole: ['', [Validators.required]],
+      fieldNewsletter: ['', [Validators.required]],
     });
   }
 
   ngOnInit(): void {
+    if (this.isAddMode) {
+      return;
+    }
     if (!this.user) {
       setTimeout(() => {
         this.retrieveEditUser.emit();
@@ -46,6 +56,10 @@ export class UsersFormComponent implements OnInit, OnChanges {
       this.form.controls.fieldEmail.patchValue(this.user.email);
       this.form.controls.fieldFirstName.patchValue(this.user.firstName);
       this.form.controls.fieldLastName.patchValue(this.user.lastName);
+      this.form.controls.fieldBirthDate.patchValue(this.user.birthDate);
+      this.form.controls.fieldPseudo.patchValue(this.user.pseudo);
+      this.form.controls.fieldRole.patchValue(this.user.role);
+      this.form.controls.fieldNewsletter.patchValue(this.user.newsletter);
     }
   }
 
@@ -54,15 +68,46 @@ export class UsersFormComponent implements OnInit, OnChanges {
       this.form.controls.fieldEmail.patchValue(this.user.email);
       this.form.controls.fieldFirstName.patchValue(this.user.firstName);
       this.form.controls.fieldLastName.patchValue(this.user.lastName);
+      this.form.controls.fieldBirthDate.patchValue(this.user.birthDate);
+      this.form.controls.fieldPseudo.patchValue(this.user.pseudo);
+      this.form.controls.fieldRole.patchValue(this.user.role);
+      this.form.controls.fieldNewsletter.patchValue(this.user.newsletter);
     }
   }
 
   save(): void {
+    if (this.isAddMode) {
+      this.saveForAdd();
+    } else {
+      this.saveForEdit();
+    }
+  }
+
+  private saveForAdd(): void {
+    const user = {
+      id: null,
+      email: this.form.value.fieldEmail,
+      firstName: this.form.value.fieldFirstName,
+      lastName: this.form.value.fieldLastName,
+      password: this.form.value.fieldPassword,
+      birthDate: this.form.value.fieldBirthDate,
+      pseudo: this.form.value.fieldPseudo,
+      role: this.form.value.fieldRole,
+      newsletter: this.form.value.fieldNewsletter,
+    };
+    this.saveUser.emit(user);
+  }
+
+  private saveForEdit(): void {
     const userProps = {
       email: this.form.value.fieldEmail,
       firstName: this.form.value.fieldFirstName,
       lastName: this.form.value.fieldLastName,
       password: this.form.value.fieldPassword,
+      birthDate: this.form.value.fieldBirthDate,
+      pseudo: this.form.value.fieldPseudo,
+      role: this.form.value.fieldRole,
+      newsletter: this.form.value.fieldNewsletter,
     };
     const newUser = { ...this.user, ...userProps };
     this.saveUser.emit(newUser);
