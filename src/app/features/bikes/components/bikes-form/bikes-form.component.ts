e8d16542dee7c2bd621224d620b29aaff64038bb
Swann <swann@devloup.dev>
2021-06-27 13:17:45 +0200

    add forms

--- src/app/features/bikes/components/bikes-form/bikes-form.component.ts
+++ src/app/features/bikes/components/bikes-form/bikes-form.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-bikes-form',
   templateUrl: './bikes-form.component.html',
-  styleUrls: ['./bikes-form.component.scss']
+  styleUrls: ['./bikes-form.component.scss'],
 })
 export class BikesFormComponent implements OnInit {
+  form: FormGroup;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  save(): void {}
 }
--- src/app/features/bikes/components/bikes-model-form/bikes-model-form.component.ts
+++ src/app/features/bikes/components/bikes-model-form/bikes-model-form.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-bikes-model-form',
   templateUrl: './bikes-model-form.component.html',
-  styleUrls: ['./bikes-model-form.component.scss']
+  styleUrls: ['./bikes-model-form.component.scss'],
 })
 export class BikesModelFormComponent implements OnInit {
+  form: FormGroup;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
+  save() {}
 }
