f5fd6714c37dd00011bee29dee284d2cdcf9470c
Nouu <noelarrieulacoste@yahoo.fr>
2021-07-04 16:58:04 +0200

    add subscription-list and plan-list component

--- src/app/features/subscriptions/subscriptions.module.ts
+++ src/app/features/subscriptions/subscriptions.module.ts
@@ -2,14 +2,20 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SubscriptionsComponent} from './containers/subscriptions/subscriptions.component';
 import {SubscriptionsRoutingModule} from './subscriptions-routing.module';
+import { PlanListComponent } from './components/plan-list/plan-list.component';
+import { SubscriptionListComponent } from './components/subscription-list/subscription-list.component';
+import {SharedModule} from "../../shared/shared.module";
 
 
 @NgModule({
   declarations: [
-    SubscriptionsComponent
+    SubscriptionsComponent,
+    PlanListComponent,
+    SubscriptionListComponent
   ],
   imports: [
     CommonModule,
+    SharedModule,
     SubscriptionsRoutingModule
   ]
 })
