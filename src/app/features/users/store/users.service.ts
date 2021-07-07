92ecac72c748a12632e295f4db2738aff4d2de8e
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2021-07-07 16:39:20 +0200

    handle user list search bar on container & service

--- src/app/features/users/store/users.service.ts
+++ src/app/features/users/store/users.service.ts
@@ -5,24 +5,26 @@ import {Snackbar} from '../../../shared/snackbar/snakbar';
 import {PutUser, RegisterUser, User} from '../../../shared/models/user.model';
 import {API_RESSOURCE_URI} from '../../../shared/api-ressource-uri/api-ressource-uri';
 import {UsersQuery} from './users.query';
+import {HttpTools} from "../../../shared/http-tools/http-tools";
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class UsersService {
   constructor(
     private usersStore: UsersStore,
     private http: HttpClientWrapper,
     private snackBar: Snackbar,
     private usersQuery: UsersQuery
-  ) {}
+  ) {
+  }
 
-  async getUsers(limit: number, offset: number): Promise<void> {
+  async getUsers(limit: number, offset: number, searchQuery: string): Promise<void> {
     this.usersStore.setLoading(true);
     try {
       const response = await this.http.get<{ users: User[]; count: number }>(
-        API_RESSOURCE_URI.GET_USERS + `?limit=${limit}&offset=${offset}`
+        API_RESSOURCE_URI.GET_USERS + '?' + HttpTools.ObjectToHttpParams({limit, offset, searchQuery})
       );
       this.usersStore.set(response.users);
-      this.usersStore.update({ count: response.count }); //TODO change response and add total
+      this.usersStore.update({count: response.count});
     } catch (e) {
       this.usersStore.set([]);
       this.snackBar.warnning(
@@ -39,7 +41,7 @@ export class UsersService {
       const response = await this.http.get<{ user: User }>(
         API_RESSOURCE_URI.GET_CURRENT_USER
       );
-      this.usersStore.update({ editUser: response.user });
+      this.usersStore.update({editUser: response.user});
     } catch (e) {
       this.snackBar.warnning(
         "Erreur lors de la récupération de l'utilisateur : " + e.error.error
@@ -70,7 +72,7 @@ export class UsersService {
         API_RESSOURCE_URI.PUT_USER + id,
         user
       );
-      this.usersStore.update({ editUser: response });
+      this.usersStore.update({editUser: response});
     } catch (e) {
       this.snackBar.warnning(
         "Erreur lors de la modification de l'utilisateur : " + e.error.error
@@ -87,7 +89,7 @@ export class UsersService {
         API_RESSOURCE_URI.POST_USER,
         user
       );
-      this.usersStore.update({ editUser: response });
+      this.usersStore.update({editUser: response});
     } catch (e) {
       this.snackBar.warnning(
         "Erreur lors de l'ajout de l'utilisateur : " + e.error.error
