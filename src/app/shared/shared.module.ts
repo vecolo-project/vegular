e6f9a5a9f8032621337663e6ffebc85e9cd5dfaa
Nouu <noelarrieulacoste@yahoo.fr>
2021-07-10 17:12:06 +0200

    station search component + implementation in bike form

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,13 +7,14 @@ import {ChartsModule} from "ng2-charts";
 import {AddressSearchComponent} from "./address-search/address-search.component";
 import {ConfirmDialogComponent} from './confirm-dialog/confirm-dialog.component';
 import { UserSearchComponent } from './user-search/user-search.component';
+import { StationSearchComponent } from './station-search/station-search.component';
 
 export function playerFactory() {
   return player;
 }
 
 @NgModule({
-  declarations: [AddressSearchComponent, ConfirmDialogComponent, UserSearchComponent],
+  declarations: [AddressSearchComponent, ConfirmDialogComponent, UserSearchComponent, StationSearchComponent],
   imports: [
     CommonModule,
     MaterialModule,
@@ -27,7 +28,8 @@ export function playerFactory() {
         ChartsModule,
         AddressSearchComponent,
         ConfirmDialogComponent,
-        UserSearchComponent
+        UserSearchComponent,
+        StationSearchComponent
     ]
 })
 export class SharedModule {
