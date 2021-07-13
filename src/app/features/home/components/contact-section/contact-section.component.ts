486d7cc7b2ebe85db600296d999da81cafb60912
Swann <swann@devloup.dev>
2021-07-13 17:53:25 +0200

    fix form contact

--- src/app/features/home/components/contact-section/contact-section.component.ts
+++ src/app/features/home/components/contact-section/contact-section.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-section',
   templateUrl: './contact-section.component.html',
-  styleUrls: ['./contact-section.component.scss']
+  styleUrls: ['./contact-section.component.scss'],
 })
 export class ContactSectionComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  contactForm: FormGroup;
+  constructor(@Inject(FormBuilder) fb) {
+    this.contactForm = fb.group({
+      FIRST_NAME: ['', [Validators.required]],
+      LAST_NAME: ['', [Validators.required]],
+      EMAIL: ['', [Validators.required]],
+      COMPANY: ['', [Validators.required]],
+      PHONE: ['', [Validators.required]],
+      CONTENT: ['', [Validators.required]],
+    });
   }
 
+  ngOnInit(): void {}
 }
