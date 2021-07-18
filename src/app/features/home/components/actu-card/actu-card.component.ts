6a3bf74a5f8a073f4e60e3b967130211cb078b16
Nouu <noelarrieulacoste@yahoo.fr>
2021-07-18 19:15:08 +0200

    homepage completed

--- src/app/features/home/components/actu-card/actu-card.component.ts
+++ src/app/features/home/components/actu-card/actu-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-actu-card',
@@ -15,7 +15,12 @@ export class ActuCardComponent implements OnInit {
     author: string;
     lectureTime: number;
     profileUrl: string;
+    content: string;
   };
-  constructor() {}
-  ngOnInit(): void {}
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
 }
